e6ab72881e899bd300be7d458733965c72cd8485
maxs <maxs@uef.fi>
2024-03-07 13:14:27 +0200

    Updated taantuvat data set

--- data-raw/taantuvat.R
+++ data-raw/taantuvat.R
@@ -11,7 +11,7 @@ load("data/vaestonmuutos.rda")
 # Classification based on 2010-2022 population change
 taantuvat <- vaestonmuutos |>
   group_by(kunta) |>
-  summarise(kokmuutos_2010_2022 = sum(kokmuutos, na.rm = TRUE), vaesto = mean(vaesto, na.rm = TRUE)) |>
+  summarise(kokmuutos_2010_2022 = sum(kokmuutos, na.rm = TRUE), vaesto = first(vaesto)) |>
   mutate(suht_muutos_2010_2022 = (kokmuutos_2010_2022 / vaesto)*100) |>
   mutate(luokka = cut(suht_muutos_2010_2022,
                       breaks = c(-Inf, -10, -5, 0, 5, 10, Inf),
